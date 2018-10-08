54b6bac07b26c8aa57d9f67be92bab05eb68419e
Joe Turner <joe@oampo.co.uk>
2018-10-08 16:53:14 +0100

    Solution

--- src/components/app.js
+++ src/components/app.js
@@ -1,11 +1,12 @@
 import React from 'react';
+import {Route} from 'react-router-dom';
 import AboutMenu from './about-menu';
 
 export default function App(props) {
     return (
         <div className="app">
             {/* This should only show up on /about/foo, /about/bar, or /about/foo/bar */}
-            <AboutMenu />
+            <Route path="/about/:meta" component={AboutMenu} />
         </div>
     );
 }
