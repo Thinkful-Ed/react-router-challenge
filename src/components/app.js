069fcb13e47c2ecaac6c18a7d96d6731278f5f4a
Ariana Faraday <ariana@thinkful.com>
2019-08-29 17:03:12 -0400

    Use simple React setup

--- src/components/app.js
+++ src/components/app.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import AboutMenu from './about-menu';
+import {hot} from "react-hot-loader";
 
-export default function App(props) {
-    return (
-        <div className="app">
-            {/* This should only show up on /about/foo, /about/bar, or /about/foo/bar */}
-            <AboutMenu />
-        </div>
-    );
+class App extends React.Component {
+    render () {
+        return (
+            <div className="app">
+                {/* This should only show up on /about/foo, /about/bar, or /about/foo/bar */}
+                <AboutMenu />
+            </div>
+        );
+    }
 }
+
+export default hot(module)(App);
