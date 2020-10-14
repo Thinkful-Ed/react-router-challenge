cf795bc61a939f3bc7f84529d44e2e431e4ba5ae
Joe Turner <jturner@thinkful.com>
2020-10-14 19:22:57 +0100

    Update app.js

--- src/components/app.js
+++ src/components/app.js
@@ -5,7 +5,7 @@ import AboutMenu from './about-menu';
 export default function App(props) {
     return (
         <div className="app">
-            {/* This should only show up on /about/foo, /about/bar, or /about/foo/bar */}
+            {/* This should show up on /about/foo, /about/bar, or /about/foo/bar */}
             <Route path="/about/:meta" component={AboutMenu} />
         </div>
     );
